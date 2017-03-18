b1bd705fb908105ac5a327e2c500f3c6230f86d1
dumeng <dumeng81@outlook.com>
2017-03-18 17:39:36 +0800

    fix jquery

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var post = $('#post')
 
 $('.ajax').on('click', function(e) {
-  var name = e.target.data("post-name")
+  var postEle = $(e.target)
+  var name = postEle.data("post-name")
   $.ajax({
     method: 'GET',
     url: 'https://raw.githubusercontent.com/dum3ng/test/master/python/'+name+'.md',
